80859a79050036140df29799e4147ab218b74acb
truongson501190-sys <truongson501190@gmail.com>
2026-04-11 11:33:30 +0700

    fix PWA full

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("SW OK"))
+      .catch(() => console.log("SW lỗi"));
+  });
+}
