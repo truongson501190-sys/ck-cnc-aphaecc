843b3b5557ec200c024f4ec17cab432adf3e3fe4
truongson501190-sys <truongson501190@gmail.com>
2026-04-11 11:40:20 +0700

    add manifest

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js")
-      .then(() => console.log("SW OK"))
-      .catch(() => console.log("SW lỗi"));
-  });
-}
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
