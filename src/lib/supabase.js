4439a2e5a049dd93714249b5d4915f084281138f
truongson501190-sys <truongson501190@gmail.com>
2026-05-14 16:48:59 +0700

    connect supabase

---
+++ src/lib/supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl =
+  'https://oxolhlkfezihtvtyjyxf.supabase.co'
+
+const supabaseKey =
+  'sb_publishable_TcBlZnBSbYU2gcM193iUaQ_UNrvMgNr'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
