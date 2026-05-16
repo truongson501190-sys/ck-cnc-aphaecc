54b57fb392a4c028ab574fee4d4481403cd82bdf
truongson501190-sys <truongson501190@gmail.com>
2026-05-16 16:20:37 +0700

    fix supabase env config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://oxolhlkfezihtvtyjyxf.supabase.co'
-
-const supabaseKey = 'sb_publishable_TcBlZnBSbYU2gcM193iUaQ_UNrvMgNr'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
